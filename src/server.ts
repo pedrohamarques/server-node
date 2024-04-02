d56e17082048a056a632f2ac9dfa8a694f7f1f47
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-04-02 11:28:00 -0300

    feat: add fastify

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/", () => {
+    return "Hello NLW Unite";
+});
+
+app.listen({ port: 3333 }).then(() => {
+    console.log("HTTP server running!");
+});
