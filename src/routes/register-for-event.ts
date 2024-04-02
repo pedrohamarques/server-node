585f72f33f33d33c330805d8c4a87aa5f554d787
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-04-02 15:26:30 -0300

    feat: create register for events route

---
+++ src/routes/register-for-event.ts
@@ -0,0 +1,77 @@
+import type { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+
+export async function registerForEvent(app: FastifyInstance) {
+    app.withTypeProvider<ZodTypeProvider>().post(
+        "/events/:eventId/attendees",
+        {
+            schema: {
+                body: z.object({
+                    name: z.string().min(4),
+                    email: z.string().email(),
+                }),
+                params: z.object({
+                    eventId: z.string().uuid(),
+                }),
+                response: {
+                    201: z.object({
+                        attendeeId: z.number(),
+                    }),
+                },
+            },
+        },
+        async (request, reply) => {
+            const { eventId } = request.params;
+            const { name, email } = request.body;
+
+            const attendeeFromEmail = await prisma.attendee.findUnique({
+                where: {
+                    eventId_email: {
+                        email,
+                        eventId,
+                    },
+                },
+            });
+
+            if (attendeeFromEmail !== null) {
+                throw new Error(
+                    "This e-mail is already registered for this event",
+                );
+            }
+
+            const [event, amountOfAttendeeForEvent] = await Promise.all([
+                prisma.event.findUnique({
+                    where: {
+                        id: eventId,
+                    },
+                }),
+                prisma.attendee.count({
+                    where: {
+                        eventId,
+                    },
+                }),
+            ]);
+
+            if (
+                event?.maximumAttendees &&
+                amountOfAttendeeForEvent >= event.maximumAttendees
+            ) {
+                throw new Error(
+                    "The maximum number of attendees for this event has already been reached.",
+                );
+            }
+
+            const attendee = await prisma.attendee.create({
+                data: {
+                    name,
+                    email,
+                    eventId,
+                },
+            });
+
+            return reply.status(201).send({ attendeeId: attendee.id });
+        },
+    );
+}
